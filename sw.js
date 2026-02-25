fa739d282524d5dd45125e1daf79eb789710a9d8
ox0114xo <ox0114xo@gmail.com>
2026-02-25 20:17:14 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,15 @@
-const CACHE_NAME = 'jp-speech-app-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+const CACHE_NAME = 'jp-voice-v4.5';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://raw.githubusercontent.com/ox0114xo/Japan/refs/heads/main/IMG_20260225_194735.png'
 ];
 
-// 安裝 Service Worker 並快取核心檔案
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-// 攔截網路請求，若有快取則返回快取，否則發送網路請求
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
