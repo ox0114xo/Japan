878a532eebb5c889c1e7d28590912814ea2e8b2b
ox0114xo <ox0114xo@gmail.com>
2026-02-25 21:00:02 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,23 @@
-const CACHE_NAME = 'jp-voice-v5';
+const CACHE_NAME = 'jp-voice-v8.5';
 const ASSETS = [
   'index.html',
   'manifest.json',
   'https://raw.githubusercontent.com/ox0114xo/Japan/refs/heads/main/IMG_20260225_194735.png'
 ];
 
-// 安裝時快取檔案
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-// 啟動時清理舊快取
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.map((key) => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      }));
-    })
+    caches.keys().then((keys) => Promise.all(keys.map((key) => {
+      if (key !== CACHE_NAME) return caches.delete(key);
+    })))
   );
 });
 
-// 必須有 fetch 事件，Chrome 才會認為是合格的 PWA
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
 });
-
